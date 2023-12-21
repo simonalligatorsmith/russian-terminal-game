4c6dacfa10d64b7958d1da4af158cd435877d432
simonalligatorsmith <simonalligatorsmith@gmail.com>
2023-12-21 08:11:18 -0600

    pronouns in six cases

--- pronouns.py
+++ pronouns.py
@@ -9,6 +9,23 @@ from os import system
 nom_pros = dict(zip(['case_name','sg_1', 'sg_2', 'sg_3_m', 'sg_3_f', 'sg_3_x', 'pl_1', 'pl_2', 'pl_3'],
                     ['Nominative','я','ты','он','она','оно','мы','вы','они']))
 
+gen_pros = dict(zip(['case_name','sg_1', 'sg_2', 'sg_3_m', 'sg_3_f', 'sg_3_x', 'pl_1', 'pl_2', 'pl_3'],
+                    ['Genitive','меня','тебя','его','ёо','его','нас','вас','их']))
+
+dat_pros = dict(zip(['case_name','sg_1', 'sg_2', 'sg_3_m', 'sg_3_f', 'sg_3_x', 'pl_1', 'pl_2', 'pl_3'],
+                    ['Dative','мне','тебе','ему','ей','ему','нам','вам','им']))
+
+acc_pros = dict(zip(['case_name','sg_1', 'sg_2', 'sg_3_m', 'sg_3_f', 'sg_3_x', 'pl_1', 'pl_2', 'pl_3'],
+                    ['Accusative','меня','тебя','его','её','его','нас','вас','их']))
+
+ins_pros = dict(zip(['case_name','sg_1', 'sg_2', 'sg_3_m', 'sg_3_f', 'sg_3_x', 'pl_1', 'pl_2', 'pl_3'],
+                    ['Instrumental','мной','тобой','им','ею','им','нами','вами','ими']))
+
+pre_pros = dict(zip(['case_name','sg_1', 'sg_2', 'sg_3_m', 'sg_3_f', 'sg_3_x', 'pl_1', 'pl_2', 'pl_3'],
+                    ['Prepositional','мне','тебе','нём','ней','нём','нас','вас','них']))
+
+pros_list = [nom_pros,gen_pros,dat_pros,acc_pros,ins_pros,pre_pros]
+
 # print out a pronoun table with one pronoun missing
 # return the missing pronoun, which will be used by the game loop
 def show_game_table(pronoun_dict):
@@ -41,7 +58,13 @@ def show_game_table(pronoun_dict):
 #show_table(nom_pros)
 
 while True:
-    missing_pronoun = show_game_table(nom_pros.copy())
+    #clear screen
+    system('clear')
+
+    # select a random pronoun table index
+    pron_index = sample(range(0,6),1)[0]
+
+    missing_pronoun = show_game_table(pros_list[pron_index].copy())
     guess = input("\n\nType the missing pronoun: ")
 
     if guess == missing_pronoun:
@@ -53,7 +76,3 @@ while True:
 
     else:
         input("Not quite.\nPress *enter* to try again")
-
-
-    #clear screen
-    system('clear')
